26c6324156d663e7d9158c39b1f4084be5e021fa
Margaret Star <imdrumcan@gmail.com>
2018-05-05 00:01:13 +0200

    changed ruby method name; widen -> widen_char, to align it with the others

--- fullwidth.rb
+++ fullwidth.rb
@@ -4,9 +4,9 @@ exit if ARGV.empty?
 
 # Returns the fullwidth version of a character.
 # If no fullwidth version exists, returns the character.
-def widen(c)
+def widen_char(c)
   c += 65248 if c.between? 33, 126 # character w/ fullwidth equivalent
   c.chr Encoding::UTF_8
 end
 
-puts ARGV.join(' ').codepoints.map { |c| widen c }.join
+puts ARGV.join(' ').codepoints.map { |c| widen_char c }.join
